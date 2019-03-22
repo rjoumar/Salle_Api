664103de07727372b373f7491d12c425f9caf7a3
Rajae JOUMAR <rjoumar@norsys.fr>
2019-03-22 15:58:10 +0000

    adding test

--- src/test/java/fr/norsys/sinaf/SalleApi/SalleApiTest.java
+++ src/test/java/fr/norsys/sinaf/SalleApi/SalleApiTest.java
@@ -1,38 +1,18 @@
 package fr.norsys.sinaf.SalleApi;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
 
 /**
  * Unit test for simple App.
  */
 public class SalleApiTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public SalleApiTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( SalleApiTest.class );
-    }
+	@Test
+	public void shouldgetNothing() {
+		assertTrue(true);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void SalleApiApp()
-    {
-        assertTrue( true );
-    }
 }
